47655a12280e8ca2fcfb41acf6ae4bdb4f12ba32
Utkarsh Kuchhal <kuchhalutkarsh17@gmail.com>
2024-06-28 00:15:23 +0530

    Search operation in redis Optimised

--- cooking_recipe_finder/recipes/views.py
+++ cooking_recipe_finder/recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.cache import cache
 from .models import Recipe
 import json
@@ -9,12 +9,15 @@ def home(request):
 def search_results(request):
     query = request.GET.get('q')
     if query:
-        recipes = cache.get(query)
-        if not recipes:
-            recipes = Recipe.objects(name__icontains=query)
-            cache.set(query, recipes.to_json(), timeout=60*15)  # Cache for 15 minutes
+        cached_result = cache.get(f"search:{query}")
+        if cached_result:
+            recipe_ids = json.loads(cached_result)
+            recipes = [json.loads(cache.get(f"recipe:{recipe_id}")) for recipe_id in recipe_ids if cache.get(f"recipe:{recipe_id}")]
         else:
-            recipes = json.loads(recipes)
+            recipes = Recipe.objects(name__icontains=query)
+            cache.set(f"search:{query}", json.dumps([str(recipe.id) for recipe in recipes]), timeout=60*15)  # Cache for 15 minutes
+            for recipe in recipes:
+                cache.set(f"recipe:{recipe.id}", recipe.to_json(), timeout=60*15)
     else:
         recipes = []
     return render(request, 'recipes/search_results.html', {'recipes': recipes})
